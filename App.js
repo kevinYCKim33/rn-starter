5e26cae67ca7857ef0a3428aa11b1f1b3a8d15a3
Kevin <kevinyckim33@gmail.com>
2021-03-16 22:04:09 -0400

    Finish Section 3

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 // this is different from other course; it might be a bit outdated...
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
+    List: ListScreen,
   },
   {
-    initialRouteName: "Components", // goes to the key
+    initialRouteName: "List", // goes to the key
     defaultNavigationOptions: {
       title: "App",
     },
